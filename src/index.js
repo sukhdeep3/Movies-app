b49dbb15ed80791f2f3a6aaffba438b0a16dc28c
sukhdeepsukh589 <sukhdeepsukh589@gmail.com>
2022-10-10 05:35:45 -0400

    Subscribe to the store

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// import { legacy_createStore as createStore } from "redux";
 import { legacy_createStore as createStore } from "redux";
-
 import "./index.css";
 import App from "./components/App";
 import movies from "./reducers";
+// import { createStore } from "redux";
 
 const store = createStore(movies);
-console.log(store);
-console.log(store.getState());
+// const store = createStore(movies);
+// console.log(store);
+// console.log(store.getState());
 
-store.dispatch({
-  type: "ADD_MOVIES",
-  movies: [{ name: "don" }],
-});
+// store.dispatch({
+//   type: "ADD_MOVIES",
+//   movies: [{ name: "don" }],
+// });
 
-console.log("after State", store.getState());
+// console.log("after State", store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <App />
-);
+root.render(<App store={store} />);
