ec8341ae333bf278e63ed7ae34dc82f40a70cc63
sukhdeepsukh589 <sukhdeepsukh589@gmail.com>
2022-10-10 02:12:21 -0400

    Setup UI of the project

---
+++ src/components/MovieCard.js
@@ -0,0 +1,26 @@
+import { render } from "@testing-library/react";
+import React from "react";
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    console.log(movie);
+    return (
+      <div className="movie-card">
+        <div className="left">
+          <img alt="movie-poster" src={movie.Poster} />
+        </div>
+        <div className="right">
+          <div className="title">{movie.Title}</div>
+          <div className="title">{movie.Plot}</div>
+          <div className="footer">
+            <div className="rating">{movie.imdbRating}</div>
+            <button className="favourite-btn">Favourite</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
