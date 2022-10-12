9a77bb7ecd3becd65287e3286d6512a32e4f1334
sukhdeepsukh589 <sukhdeepsukh589@gmail.com>
2022-10-12 03:44:56 -0400

    Instead of root reducers, combineReducer from redux is introduced.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,4 @@
+import { combineReducers } from "redux";
 import {
   ADD_TO_FAVOURITES,
   ADD_MOVIES,
@@ -56,9 +57,14 @@ const initialRootState = {
   search: initialSearchState,
 };
 
-export default function rootReducer(state = initialRootState, action) {
-  return {
-    movies: movies(state.movies, action),
-    search: search(state.search, action),
-  };
-}
+// export default function rootReducer(state = initialRootState, action) {
+//   return {
+//     movies: movies(state.movies, action),
+//     search: search(state.search, action),
+//   };
+// }
+
+export default combineReducers({
+  movies,
+  search,
+});
