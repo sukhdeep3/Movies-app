4af29109525cb3e983dfef0cc70a1dc93246259b
sukhdeepsukh589 <sukhdeepsukh589@gmail.com>
2022-10-15 05:09:07 -0400

    Connect App components

--- src/components/App.js
+++ src/components/App.js
@@ -81,14 +81,22 @@ class App extends React.Component {
   }
 }
 
-class AppWrapper extends React.Component{
-  render(){
-    return(
-      <StoreContext.Consumer>
-        {(store)=> <App store={store}/>}
-      </StoreContext.Consumer>
-    )
-  }
-}
+// class AppWrapper extends React.Component {
+//   render() {
+//     return (
+//       <StoreContext.Consumer>
+//         {(store) => <App store={store} />}
+//       </StoreContext.Consumer>
+//     );
+//   }
+// }
 
-export default AppWrapper;
+function callback(state) {
+  return {
+    movies: state.movies,
+    search: state.search,
+  };
+}
+const connectedAppComponent = connect(callback)(App);
+
+export default connectedAppComponent;
